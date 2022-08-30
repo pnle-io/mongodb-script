101d49f48de4e6c4edfc6e578ac56d2c07426c18
Phong Le <phonglnk@gmail.com>
2022-08-30 16:07:55 +0800

    Commit code repos and update README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# mongodb-script
+# Query all collection in all database, excepting views collection and system, local, config databases
+
+## Structure
+- getCollStats.py: Loop through all defined cluster connection, query all collection at all databases in the cluster to get the statistics
+- postCollStats.py: Shell script which uses mongoimport to load statistics data to another MongoDB collection for further analysis
+
+## How to execute:
+1. Update the file getCollStats.py to define the list of connection string to query the collection stats
+2. Update the file postCollStats.sh to use mongoimport to import data to an MongoDB collection for further analysis
+3. Use MongoDB Compass or CLI to query statistics data
---
+++ getCollStats.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+import json
+
+conn_str_0 = "<connection-string-cluster-1>"
+conn_str_1 = "<connection-string-cluster-2>"
+conn_str_2 = "<connection-string-cluster-3>"
+
+conn_pool = [conn_str_0, conn_str_1, conn_str_2]
+output = []
+
+for conn in conn_pool:
+    client = client = MongoClient(conn)
+    for db in client.list_database_names():
+        if db not in ["admin", "config", "local"]:
+            for coll in client[db].list_collections():            
+                if (coll["name"] not in [ "system.views" ]) and ( coll["type"] != "view" ):
+                    coll_stat = {
+                        "db_name": db,
+                        "coll_name": coll["name"],
+                        "coll_size_MB": client[db].command("collStats", coll["name"], scale = 1024*1024 )["size"],
+                        "avg_obj_size_MB": client[db].command("collStats", coll["name"], scale = 1024*1024 )["avgObjSize"],
+                        "num_doc": client[db].command("collStats", coll["name"])["count"],
+                        "storage_size_MB": client[db].command("collStats", coll["name"], scale = 1024*1024 )["storageSize"],
+                        "num_idx": client[db].command("collStats", coll["name"])["nindexes"],
+                        "idx_size_MB": client[db].command("collStats", coll["name"], scale = 1024*1024 )["totalIndexSize"],
+                        "total_size_MB": client[db].command("collStats", coll["name"], scale = 1024*1024 )["totalSize"],
+                    }
+                    output.append(coll_stat)
+    client.close()
+print(json.dumps(output))
+
