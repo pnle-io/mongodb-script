fc3d3670dfbb9b15e48bde5debb99e128b9f1e77
cheelim1 <98261283+cheelim1@users.noreply.github.com>
2022-08-31 10:14:14 +0800

    Update filename

--- get_collection_stats/postCollStats.sh
+++ get_collection_stats/postCollStats.sh
@@ -1,5 +1,5 @@
 echo "Collect stats from all collections in all database..."
-python3 getCollectionStatsByAllDb.py > outputCollStats.json
+python3 getCollStats.py.py > outputCollStats.json
 echo "Import to MongoDB collection..."
 mongoimport --uri mongodb+srv://<user-name>:<password>@<mongodb-srv-uri>/<db-name> --collection <collection-name> --type JSON --file outputCollStats.json --jsonArray --drop
 echo "Import completed!"
